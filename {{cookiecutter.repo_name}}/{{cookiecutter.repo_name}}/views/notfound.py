b3d7bf3e2771971c0caa911fd161ca72b9d05458
Diwaker Ghimire <idwaker@gmail.com>
2017-11-16 13:15:15 +0545

    fix minor syntax errors

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/views/notfound.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/views/notfound.py
@@ -1,7 +1,10 @@
 from pyramid.view import notfound_view_config
 
 
-@notfound_view_config(renderer='../templates/404.jinja2')
+@notfound_view_config(renderer='json')
 def notfound_view(request):
     request.response.status = 404
-    return {}
+    return {
+        'status': 404,
+        'message': '404 Not Found'
+    }
