fe381faae35e2e8817bae67d8d0a0cc57f1c277d
Diwaker Ghimire <idwaker@gmail.com>
2017-11-16 09:21:51 +0545

    migrating to config.cfg format

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,54 +1,8 @@
-import os
+import setuptools
 
-from setuptools import setup, find_packages
+assert setuptools.__version__ >= '30.3', "Requires latest version of setuptools"
 
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
-    README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
-    CHANGES = f.read()
-
-requires = [
-    'plaster_pastedeploy',
-    'pyramid >= 1.9a',
-    'pyramid_debugtoolbar',
-    'pyramid_jinja2',
-    'pyramid_retry',
-    'pyramid_tm',
-    'SQLAlchemy',
-    'transaction',
-    'zope.sqlalchemy',
-    'waitress',
-]
-
-tests_require = [
-    'WebTest >= 1.3.1',  # py3 compat
-    'pytest',
-    'pytest-cov',
-]
-
-setup(
-    name='{{ cookiecutter.repo_name }}',
-    version='0.0',
-    description='{{ cookiecutter.project_name }}',
-    long_description=README + '\n\n' + CHANGES,
-    classifiers=[
-        'Programming Language :: Python',
-        'Framework :: Pyramid',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
-    ],
-    author='',
-    author_email='',
-    url='',
-    keywords='web pyramid pylons',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    extras_require={
-        'testing': tests_require,
-    },
-    install_requires=requires,
+setuptools.setup(
     entry_points={
         'paste.app_factory': [
             'main = {{ cookiecutter.repo_name }}:main',
