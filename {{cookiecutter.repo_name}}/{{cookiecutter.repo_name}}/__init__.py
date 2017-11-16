a367def410b00eac0c43e075b97b66743f9a7c03
Diwaker Ghimire <idwaker@gmail.com>
2017-11-16 16:54:08 +0545

    added pyramid_jwt

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
@@ -1,4 +1,8 @@
+from os import environ
+
 from pyramid.config import Configurator
+from pyramid.authorization import ACLAuthorizationPolicy
+
 from {{cookiecutter.repo_name}}.managers import init_logging
 
 
@@ -9,6 +13,12 @@ def main(global_config, **settings):
 
     # initialize services
     init_logging(config)
+
+    # set authorization policy and enable JWT authentication
+    config.set_authorization_policy(ACLAuthorizationPolicy())
+    config.include('pyramid_jwt')
+    # key-accessor is there so that it throws exception if the key is not set
+    config.set_jwt_authentication_policy(environ['{{cookiecutter.repo_name.upper()}}_SECRET_KEY'])
 
     # includes
     config.include('cornice')
